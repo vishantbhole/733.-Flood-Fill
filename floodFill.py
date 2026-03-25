78086037f712115fc22c673d01232fe355e66168
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-03-25 06:11:04 -0500

    Update floodFill.py

--- floodFill.py
+++ floodFill.py
@@ -1,3 +1,7 @@
 
 #733. Flood Fill
 from typing import List
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        start = image[sr][sc]
